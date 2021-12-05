ccbf1551d7706e13fa7f69dc12ff4b8cb2927f96
Bijon Mallik <bijon.ndc.pust@gmail.com>
2021-12-05 21:40:16 +0600

    Camel Case 4

--- Week01(Problem05).py
+++ Week01(Problem05).py
@@ -7,66 +7,89 @@ Created on Sat Dec  4 18:03:36 2021
 def splitString(s1):
     if s1[2]=="M":
         met=""
-        for i in range(4,len(s1)-6):
+        for i in range(4,len(s1)-2):
             if s1[i] >= 'A' and s1[i] <= 'Z':
-                s1[i]=chr(ord(s1[i])+32)
-                met=met+" "+s1[i]
+                met=met+" "+chr(ord(s1[i])+32)
+                
                 continue
             met+=s1[i]
         print(met)
     elif s1[2]=="C":
-        met=s1[0]+32
-        for i in range(5,len(s1)-5):
+        met=chr(ord(s1[4])+32)
+        for i in range(5,len(s1)):
             if s1[i] >= 'A' and s1[i] <= 'Z':
-                s1[i]=chr(ord(s1[i])+32)
-                met=met+" "+s1[i]
+                met=met+" "+chr(ord(s1[i])+32)
+                
                 continue
             met+=s1[i]
         print(met)
     else:
         met=""
-        for i in range(4,len(s1)-4):
+        for i in range(4,len(s1)):
             if s1[i] >= 'A' and s1[i] <= 'Z':
-                s1[i]=s1[i]+32
-                met=met+" "+s1[i]
+                met=met+" "+chr(ord(s1[i])+32)
+                
                 continue
             met+=s1[i]
         print(met)
                 
 def combString(c1):
+    c2=""
     if c1[2]=="V":
         
-        met=""
-        for i in range(4,len(c1)-4):
+        
+        for i in range(4,len(c1),1):
             if c1[i]==" ":
-                c1[i+1]=c1[i+1]-32
+                c2+=""
                 continue
-            met+=c1[i]
-        print(met)
+            if c1[i-1]==" ":
+                c2+=chr(ord(c1[i])-32)
+                continue
+            
+            c2+=c1[i]
+        print(c2)
     elif c1[2]=="C":
-        met=c1[0]-32
-        for i in range(5,len(c1)-5):
+        c2=chr(ord(c1[4])-32)
+        for i in range(5,len(c1)):
             if c1[i]==" ":
-                c1[i+1]=c1[i+1]-32
+                c2+=""
                 continue
-            met+=c1[i]
-        print(met)
+            if c1[i-1]==" ":
+                c2+=chr(ord(c1[i])-32)
+                continue
+            
+            c2+=c1[i]
+        print(c2)
     else:
-        met=""
-        for i in range(4,len(c1)-4):
+        
+        
+        
+        for i in range(4,len(c1),1):
+            
             if c1[i]==" ":
-                c1[i+1]=c1[i+1]-32
+                c2+=""
                 continue
-            met+=c1[i]
-        print(met+"()")
+            if c1[i-1]==" ":
+                c2+=chr(ord(c1[i])-32)
+                continue
+            
+            c2+=c1[i]
+            
+        print(c2+"()")
         
         
     
     
 if __name__=="__main__":
-    stri=input()
-    if stri[0]=="S":
-        splitString(stri)
-    elif stri[0]=="C":
-        combString(stri)
+    while True:
+        try:
+            stri=input()
+            if stri[0]=="S":
+                splitString(stri)
+            elif stri[0]=="C":
+                combString(stri)
+            else:
+                break
+        except (EOFError):
+            break
     
--- test.py
+++ test.py
@@ -4,4 +4,8 @@ Created on Sat Dec  4 20:30:49 2021
 
 @author: Admin
 """
-print(chr(ord('B')+32))
+for _ in range():
+    hjk =""
+    b="hjkk"
+    hjk=b
+    print(hjk)
