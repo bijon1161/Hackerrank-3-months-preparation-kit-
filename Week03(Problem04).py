2ae8ed17ca471e36ee89c54d6d8e347dcd042f13
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-01-31 20:43:02 +0600

    Sales by Match

---
+++ Week03(Problem04).py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 12:15:10 2022
+
+@author: Admin
+"""
+import math
+def socksMerchant(n,arr):
+    s=list(set(arr))
+    print(s)
+    res=0
+    for i in range(len(s)):
+        x=(arr.count(s[i]))
+        res+=math.floor(x/2)
+    return res
+
+if __name__=="__main__":
+    n=int(input())
+    arr=list(map(int,input().split()))
+    print(socksMerchant(n,arr))
+    
